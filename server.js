6fd2a66786389437a6a3131275bfc4ca9ef12709
xingyinyue <xingyinyue@gmail.com>
2025-12-29 12:48:18 +0800

    baby card project initial

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
